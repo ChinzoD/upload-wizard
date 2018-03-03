939747f870ccaf738d8d25b656b4f7a53a98b607
Chinzo Dandar <chinzo.dandar@gmail.com>
2018-03-02 16:51:20 -0800

    fixes bug

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ class Config(object):
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
                               'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    STATIC_FOLDER = 'static'
+    # UPLOAD_FOLDER = '/app/static/files'
     # MAIL_SERVER = os.environ.get('MAIL_SERVER')
     # MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25)
     # MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
