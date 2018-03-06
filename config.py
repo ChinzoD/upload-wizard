1bda7399fa504c3aa2535997c8ae9b14965fe0fe
Chinzo Dandar <chinzo.dandar@gmail.com>
2018-03-06 15:40:05 -0800

    file upload progress bar

--- config.py
+++ config.py
@@ -7,7 +7,10 @@ class Config(object):
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
                               'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    # UPLOAD_FOLDER = '/app/static/files'
+    POSTS_PER_PAGE = 20
+    MAX_CONTENT_LENGTH = 100 * 1024 * 1024
+    UPLOAD_FOLDER = '/mnt/nfs/work/chinzo/Projects/vendor-upload/app/static/files'
+    UPLOAD_FOLDER1 = '/app/static/files'
     # MAIL_SERVER = os.environ.get('MAIL_SERVER')
     # MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25)
     # MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
